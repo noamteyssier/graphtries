d7de3c29707b3977a08c51e8667dc676911b6cb7
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-17 17:33:02 -0700

    example using yeast

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,21 @@ use isomorphism::canonical_based_nauty;
 
 fn main() {
     let s = 3;
-    let graph_path = "example/example.txt";
+    // let graph_path = "example/example.txt";
+    let graph_path = "example/yeast.txt";
     let path = "example/dir3.g6";
     let mut gtrie = Gtrie::new(s);
+
     for graph in io::iter_graphs_from_file(path) {
         let mut bgraph = Bitgraph::from_graph(&graph);
+        let canon = canonical_based_nauty(&bgraph.adjacency(), s);
+        bgraph.overwrite_adjacency(canon);
         gtrie.insert(&bgraph);
     }
-    // gtrie.pprint();
 
     let graph = io::load_numeric_graph(graph_path, true).unwrap();
     let query = Bitgraph::from_graph(&graph);
     gtrie.census(&query);
+
+    gtrie.pprint();
 }
