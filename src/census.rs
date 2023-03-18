58af3e9c53d78c8bde6c3e5fc5d4745fd545c4bd
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-18 13:11:48 -0700

    move code blocks around to be together

--- src/census.rs
+++ src/census.rs
@@ -25,6 +25,63 @@ pub fn match_child(
     }
 }
 
+
+fn matching_vertices(
+    node: &GtrieNode,
+    used: &[usize],
+    graph: &Bitgraph,
+    candidates: &mut Candidates,
+    connections: &mut Connections,
+) -> Vec<usize> {
+    build_candidates(graph, used, candidates, connections);
+    let mut vertices = Vec::new();
+    candidates
+        .ones()
+        .filter(|v| matches_structure(node, graph, used, *v))
+        .for_each(|v| vertices.push(v));
+    clear_bits(candidates, connections);
+    vertices
+}
+
+fn build_candidates(
+    graph: &Bitgraph,
+    used: &[usize],
+    candidates: &mut Candidates,
+    connections: &mut Connections,
+) {
+    if used.len() == 0 {
+        candidates.insert_range(..);
+    } else {
+        let mut v_min = usize::MAX;
+        for v in used {
+            for n in graph.undir_neighbors(*v).ones() {
+                if used.contains(&n) {
+                    continue;
+                }
+                connections.insert(n);
+                let nn = graph.n_undir_neighbors(n);
+                if nn < v_min {
+                    v_min = nn;
+                }
+            }
+        }
+        candidates.union_with(connections);
+    }
+}
+
+fn matches_structure(node: &GtrieNode, graph: &Bitgraph, used: &[usize], v: usize) -> bool {
+    used.iter().enumerate().all(|(i, u)| {
+        node.out_contains(i) == graph.is_connected(*u, v)
+            && node.in_contains(i) == graph.is_connected(v, *u)
+    })
+}
+
+/*
+ * Conditionally match a child node.
+ * This is used for the census of the graph space.
+ * The condition is that the node must be a graph.
+*/
+
 pub fn match_child_conditionally(
     node: &mut GtrieNode,
     used: &mut Vec<usize>,
@@ -63,23 +120,6 @@ pub fn matching_vertices_conditionally(
     vertices
 }
 
-fn matching_vertices(
-    node: &GtrieNode,
-    used: &[usize],
-    graph: &Bitgraph,
-    candidates: &mut Candidates,
-    connections: &mut Connections,
-) -> Vec<usize> {
-    build_candidates(graph, used, candidates, connections);
-    let mut vertices = Vec::new();
-    candidates
-        .ones()
-        .filter(|v| matches_structure(node, graph, used, *v))
-        .for_each(|v| vertices.push(v));
-    clear_bits(candidates, connections);
-    vertices
-}
-
 fn build_candidates_conditionally(
     graph: &Bitgraph,
     used: &[usize],
@@ -107,40 +147,7 @@ fn build_candidates_conditionally(
     }
 }
 
-fn build_candidates(
-    graph: &Bitgraph,
-    used: &[usize],
-    candidates: &mut Candidates,
-    connections: &mut Connections,
-) {
-    if used.len() == 0 {
-        candidates.insert_range(..);
-    } else {
-        let mut v_min = usize::MAX;
-        for v in used {
-            for n in graph.undir_neighbors(*v).ones() {
-                if used.contains(&n) {
-                    continue;
-                }
-                connections.insert(n);
-                let nn = graph.n_undir_neighbors(n);
-                if nn < v_min {
-                    v_min = nn;
-                }
-            }
-        }
-        candidates.union_with(connections);
-    }
-}
-
 fn matches_structure_conditionally(node: &GtrieNode, graph: &Bitgraph, used: &[usize], v: usize) -> bool {
-    used.iter().enumerate().all(|(i, u)| {
-        node.out_contains(i) == graph.is_connected(*u, v)
-            && node.in_contains(i) == graph.is_connected(v, *u)
-    })
-}
-
-fn matches_structure(node: &GtrieNode, graph: &Bitgraph, used: &[usize], v: usize) -> bool {
     used.iter().enumerate().all(|(i, u)| {
         node.out_contains(i) == graph.is_connected(*u, v)
             && node.in_contains(i) == graph.is_connected(v, *u)
