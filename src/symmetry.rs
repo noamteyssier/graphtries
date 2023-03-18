bcf302d8674b61fd96df20cdc1f5ef8c4bbf1ddc
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-18 13:11:28 -0700

    added respect functions which accept depth and testing for said functions

--- src/symmetry.rs
+++ src/symmetry.rs
@@ -23,8 +23,15 @@ impl Condition {
         }
     }
 
-    pub fn is_respected(&self, u: usize, v: usize) -> bool {
-        if u == self.u && v == self.v {
+    /// Returns true if the condition is respected by the given graph
+    ///
+    /// # Arguments
+    /// * `d1` - The depth of the first vertex
+    /// * `d2` - The depth of the second vertex
+    /// * `u` - The first vertex
+    /// * `v` - The second vertex
+    pub fn is_respected(&self, d1: usize, d2: usize, u: usize, v: usize) -> bool {
+        if d1 == self.u && d2 == self.v {
             return u < v
         }
         true
@@ -59,8 +66,80 @@ impl Conditions {
         self.conditions.retain(f)
     }
 
-    pub fn respects_all(&self, u: usize, v: usize) -> bool {
-        self.conditions.iter().all(|c| c.is_respected(u, v))
+    /// Returns true if the conditions are respected for the given pair of vertices
+    ///
+    /// # Arguments
+    /// * 'd1' - The depth of the first vertex
+    /// * 'd2' - The depth of the second vertex
+    /// * 'u' - The first vertex index
+    /// * 'v' - The second vertex index
+    pub fn respects_all(&self, d1: usize, d2: usize, u: usize, v: usize) -> bool {
+        assert!(d1 <= d2, "d1 must be less than or equal to d2");
+        self.conditions.iter().all(|c| c.is_respected(d1, d2, u, v))
     }
+}
+
+#[cfg(test)]
+mod testing {
+
+    use super::*;
+
+    #[test]
+    fn condition_a() {
+        let c1 = Condition::new(0, 1);
+        let conditions = Conditions::from_vec(vec![c1]);
+        
+        let test_set = vec![
+            (0, 1, 10, 20, true),  // should pass
+            (0, 1, 20, 10, false), // automorphism, should fail
+            (1, 2, 20, 10, true),  // irrelevant, should pass
+            (1, 2, 20, 10, true),  // irrelevant, should pass
+        ];
+        for (d1, d2, u, v, expected) in test_set {
+            assert_eq!(conditions.respects_all(d1, d2, u, v), expected);
+        }
+    }
+
+    #[test]
+    fn condition_b() {
+        let c1 = Condition::new(0, 1);
+        let c2 = Condition::new(1, 2);
+        let conditions = Conditions::from_vec(vec![c1, c2]);
+        
+        let test_set = vec![
+            (0, 1, 10, 20, true),   // fixed-orbit, should pass
+            (0, 1, 20, 10, false),  // automorphism, should fail
+            (1, 2, 30, 40, true),   // fixed-orbit, should pass
+            (1, 2, 40, 30, false),  // automorphism, should pass
+        ];
+        for (d1, d2, u, v, expected) in test_set {
+            assert_eq!(conditions.respects_all(d1, d2, u, v), expected);
+        }
+    }
+
+    #[test]
+    fn condition_c() {
+        let c1 = Condition::new(0, 1);
+        let c2 = Condition::new(2, 3);
+        let conditions = Conditions::from_vec(vec![c1, c2]);
+        
+        let test_set = vec![
+            (0, 1, 10, 20, true),   // fixed-orbit, should pass
+            (0, 1, 20, 10, false),  // automorphism, should fail
+            (1, 2, 30, 40, true),   // irrelevant, should pass
+            (1, 2, 40, 30, true),   // irrelevant, should pass
+            (2, 3, 50, 60, true),   // fixed-orbit, should pass
+            (2, 3, 60, 50, false),  // automorphism, should fail
+            (3, 4, 70, 80, true),   // irrelevant, should pass
+            (3, 4, 80, 70, true),   // irrelevant, should pass
+        ];
+        for (d1, d2, u, v, expected) in test_set {
+            println!("{}<{}: {}<{}: {}", d1, d2, u, v, expected);
+            assert_eq!(conditions.respects_all(d1, d2, u, v), expected);
+        }
+    }
+
+
+
 }
 
