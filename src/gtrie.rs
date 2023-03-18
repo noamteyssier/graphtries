5c16cf132f6651b4e16ecb5437a48bcd077c09dd
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-17 18:08:20 -0700

    include debug in release

--- src/gtrie.rs
+++ src/gtrie.rs
@@ -1,6 +1,6 @@
 use fixedbitset::FixedBitSet;
 
-use crate::{bitgraph::Bitgraph, node::GtrieNode};
+use crate::{bitgraph::Bitgraph, node::GtrieNode, census::match_child};
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -61,67 +61,11 @@ impl Gtrie {
     }
 
     pub fn census(&mut self, graph: &Bitgraph) {
-        let used = Vec::new();
+        let used = Vec::with_capacity(self.max_depth);
+        let mut candidates = vec![0; graph.n_nodes()];
         for c in self.root.iter_children_mut() {
-            Self::match_child(c, &used, &graph);
+            match_child(c, &used, &mut candidates, &graph);
         }
     }
 
-    fn match_child(node: &mut GtrieNode, used: &[usize], graph: &Bitgraph) {
-        let vertices = Self::matching_vertices(node, &used, graph);
-        for v in vertices {
-            let mut used_2 = used.to_vec();
-            used_2.push(v);
-            if node.is_graph() {
-                node.increment_frequency();
-            } else {
-                for c in node.iter_children_mut() {
-                    Self::match_child(c, &used_2, graph);
-                }
-            }
-        }
-    }
-
-    fn matching_vertices(node: &GtrieNode, used: &[usize], graph: &Bitgraph) -> Vec<usize> {
-        let cand = Self::build_candidates(graph, used);
-        let mut vertices = Vec::new();
-        for v in cand {
-            let mut condition_a = true;
-            let mut condition_b = true;
-            for (i, u) in used.iter().enumerate() {
-                condition_a &= node.out_contains(i) == graph.is_connected(*u, v);
-                condition_b &= node.in_contains(i) == graph.is_connected(v, *u);
-            }
-            if condition_a && condition_b {
-                vertices.push(v);
-            }
-        }
-        vertices
-    }
-
-    fn build_candidates(graph: &Bitgraph, used: &[usize]) -> Vec<usize> {
-        if used.len() == 0 {
-            (0..graph.n_nodes()).collect()
-        } else {
-            let mut cand = Vec::new();
-            let mut v_conn = Vec::new();
-            let mut v_min = usize::MAX;
-            for v in used {
-                for n in graph.undir_neighbors(*v).ones() {
-                    if used.contains(&n) {
-                        continue;
-                    }
-                    v_conn.push(n);
-                    let nn = graph.n_undir_neighbors(n);
-                    if nn < v_min {
-                        v_min = nn;
-                    }
-                }
-            }
-            for i in v_conn {
-                cand.push(i);
-            }
-            cand
-        }
-    }
 }
