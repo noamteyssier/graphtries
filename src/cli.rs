ad358dcaa7117beae86a8daed0203b720218f6eb
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-03-21 15:11:11 -0700

    included a CLI

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub mode: Mode,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Mode {
+
+    /// Enumerate all subgraphs of a given size given a graph and a gtrie.
+    Enumerate {
+        #[arg(short, long)]
+        gtrie: String,
+
+        #[arg(short, long)]
+        input: String,
+    },
+
+    /// Build a gtrie from a list of graphs.
+    Build {
+        #[arg(short, long)]
+        input: String,
+
+        #[arg(short, long)]
+        output: Option<String>,
+
+        #[arg(short, long)]
+        size: usize,
+    },
+
+}
